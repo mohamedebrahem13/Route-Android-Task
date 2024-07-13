71edca23d1e92ef5a99b66159f7f265945cd4e1c
mohamedebrahem13 <3_202215576@alexu.edu.eg>
2024-07-13 21:36:10 +0300

    new changes in xml and add the adapter

--- app/src/main/java/com/example/route_android_task/domain/models/Review.kt
+++ app/src/main/java/com/example/route_android_task/domain/models/Review.kt
@@ -3,7 +3,7 @@ package com.example.route_android_task.domain.models
 data class Review(
     val rating: Int,
     val comment: String,
-    val date: String, // Assuming you handle nullable dates appropriately
+    val date: String,
     val reviewerName: String,
     val reviewerEmail: String
 )

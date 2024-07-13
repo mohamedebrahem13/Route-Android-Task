bb5f2f8291c5ea099776dee2fd74f0b562616aef
mohamedebrahem13 <3_202215576@alexu.edu.eg>
2024-07-13 17:03:28 +0300

    new changes in xml and add the adapter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.route_android_task"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
